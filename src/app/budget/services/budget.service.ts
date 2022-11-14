67a7f4bee78f5f0ce11282402d460ee63774120c
gautierr <romain.gautier@sib.fr>
2022-11-14 09:49:25 +0100

    [BUDGETS] Filtre uniquement les étapes administratives

--- src/app/budget/services/budget.service.ts
+++ src/app/budget/services/budget.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable, InjectionToken } from "@angular/core";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { Observable, throwError } from "rxjs";
+import { tap, map, catchError } from "rxjs/operators";
 import { SettingsService } from "src/environments/settings.service";
-import { DonneesBudgetairesDisponibles } from "../models/donnees-budgetaires-disponibles";
+import { DonneesBudgetairesDisponibles, RessourcesDisponiblesParAnnees } from "../models/donnees-budgetaires-disponibles";
 import { DonneesBudgetaires } from "../models/donnees-budgetaires";
 import { Pdc } from "../models/plan-de-comptes";
 import { EtapeBudgetaire, EtapeBudgetaireUtil } from "../models/etape-budgetaire";
@@ -31,7 +31,9 @@ export class RealBudgetService implements BudgetService {
     const url = `${this._getBudgetBaseUrl()}/donnees_budgetaires_disponibles/${siren}`
     return this.http.get<DonneesBudgetairesDisponibles>(url)
       .pipe(
-        map(map_donnees_budgetaires_disponibles_from_wire)
+        map(map_donnees_budgetaires_disponibles_from_wire),
+        map(strip_etapes_except_administratif_from_donnees_disponibles), // TODO: enlever lorsqu'on traitera autres que comptes administratifs
+        catchError(err => this.log_and_rethrow(err, `Erreur lors de la récupération/traitement des données budgetaires disponibles (siren: ${siren}): ${err}`)),
       )
   }
 
@@ -46,7 +48,8 @@ export class RealBudgetService implements BudgetService {
           informations.siret = siret;
           informations.annee = annee;
           return informations;
-        })
+        }),
+        catchError(err => this.log_and_rethrow(err, `Erreur lors de la récupération/traitement des informations PDC (annee: ${annee}, siret: ${siret}): ${err}`)),
       );
 
     return informationsPdc;
@@ -64,9 +67,22 @@ export class RealBudgetService implements BudgetService {
           let etape = EtapeBudgetaireUtil.fromApi(etape_str);
           donnees.etape = etape;
           return donnees;
-        })
+        }),
+        catchError(err => this.log_and_rethrow(err, `Erreur lors de la récupération/traitement des données budgetaires (annee: ${annee}, siret: ${siret}, etape: ${etape}): ${err}`)),
       );
     return donnees;
+  }
+
+  private log_and_rethrow(err: any, msg?: string) {
+    let _msg = msg
+    if (_msg === undefined)
+      _msg = err
+    this._error(_msg)
+    return throwError(() => err)
+  }
+
+  private _error(msg: string) {
+    console.error(`[${RealBudgetService.name}] ${msg}`)
   }
 
   private _debug(msg: string) {
@@ -95,4 +111,35 @@ export function map_donnees_budgetaires_disponibles_from_wire(wire: any): Donnee
   }
 
   return t_wire;
+}
+
+function strip_etapes_except_administratif_from_donnees_disponibles(donnees_disponibles: DonneesBudgetairesDisponibles): DonneesBudgetairesDisponibles {
+
+  console.warn(`Exclusion des étapes budgetaires mis à part l'étape administrative au sein des ressources disponibles`);
+  let new_ressources_disponibles: RessourcesDisponiblesParAnnees = {}
+
+  let ressources_disponibles = donnees_disponibles.ressources_disponibles
+
+  for (const annee in ressources_disponibles) {
+
+    let res_x_annee = {}
+
+    for (const siret in ressources_disponibles[annee]) {
+      let etapes = ressources_disponibles[annee][siret]
+
+      if (etapes.includes(EtapeBudgetaire.COMPTE_ADMINISTRATIF)) {
+        res_x_annee[siret] = [EtapeBudgetaire.COMPTE_ADMINISTRATIF]
+      }
+    }
+
+    if (Object.keys(res_x_annee).length > 0)
+      new_ressources_disponibles[annee] = res_x_annee
+  }
+
+  let donnees = {
+    infos_etablissements: donnees_disponibles.infos_etablissements,
+    ressources_disponibles: new_ressources_disponibles,
+    siren: donnees_disponibles.siren,
+  }
+  return donnees
 }
